54f3ecec4202c5f7b3939360e26a0071d8f9506c
Lucas Coelho <l.coelhooliveira@gmail.com>
2019-03-19 17:50:14 -0300

    Initial project

---
+++ src/GameOption.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GameOption extends Component{
+    render(){
+        return(
+            <div className='gameOption'>
+                <button>Start random secrect word</button>
+            </div>
+        )
+    }
+}
+
+export default GameOption
